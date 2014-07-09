7e154793f98697c5a3786227265849485b836469
evoskuil <eric@voskuil.org>
2014-07-09 17:07:20 -0600

    Pull NuGet dependencies for batch VS builds.

--- builds/msvc/build/buildall.bat
+++ builds/msvc/build/buildall.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
 ECHO.
+ECHO Downloading obelisk dependencies from NuGet
+CALL nuget.exe install ..\vs2013\obelisk\packages.config
+ECHO.
 CALL buildbase.bat ..\vs2013\obelisk.sln 12
 ECHO.
 PAUSE
