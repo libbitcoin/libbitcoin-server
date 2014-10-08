0be861ff060bfa74e2b31762c5ed242d8b80193b
evoskuil <eric@voskuil.org>
2014-10-07 23:35:10 -0700

    Rename libbitcoin_server to libbitcoin-server.

--- builds/msvc/build/buildall.bat
+++ builds/msvc/build/buildall.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 ECHO.
-ECHO Downloading libbitcoin_server dependencies from NuGet
-CALL nuget.exe install ..\vs2013\libbitcoin_server\packages.config
+ECHO Downloading libbitcoin-server dependencies from NuGet
+CALL nuget.exe install ..\vs2013\libbitcoin-server\packages.config
 ECHO.
-CALL buildbase.bat ..\vs2013\libbitcoin_server.sln 12
+CALL buildbase.bat ..\vs2013\libbitcoin-server.sln 12
 ECHO.
 PAUSE
