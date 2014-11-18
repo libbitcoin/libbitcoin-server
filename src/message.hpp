70a29eeee4e69e760cba84f562f514ee67a182c2
Amir Taaki <genjix@riseup.net>
2014-11-18 20:54:44 +0000

    add missing header.

---
+++ src/message.hpp
@@ -0,0 +1,50 @@
+#ifndef OBELISK_CLIENT_MESSAGE
+#define OBELISK_CLIENT_MESSAGE
+
+#include <czmq++/czmq.hpp>
+#include <bitcoin/bitcoin.hpp>
+
+namespace obelisk {
+
+class incoming_message
+{
+public:
+    bool recv(czmqpp::socket& socket);
+    const bc::data_chunk origin() const;
+    const std::string& command() const;
+    uint32_t id() const;
+    const bc::data_chunk& data() const;
+
+private:
+    bc::data_chunk origin_;
+    std::string command_;
+    uint32_t id_;
+    bc::data_chunk data_;
+};
+
+class outgoing_message
+{
+public:
+    // Empty dest = unspecified destination.
+    outgoing_message(
+        const bc::data_chunk& dest, const std::string& command,
+        const bc::data_chunk& data);
+    outgoing_message(
+        const incoming_message& request, const bc::data_chunk& data);
+    // Default constructor provided for containers and copying.
+    outgoing_message();
+
+    void send(czmqpp::socket& socket) const;
+    uint32_t id() const;
+
+private:
+    bc::data_chunk dest_;
+    std::string command_;
+    uint32_t id_;
+    bc::data_chunk data_;
+};
+
+} // namespace obelisk
+
+#endif
+
