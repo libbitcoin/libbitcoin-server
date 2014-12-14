c3a3e1792b053808457ea5afe3bbeb6795420a9e
evoskuil <eric@voskuil.org>
2014-12-13 18:01:10 -0800

    Install from protocol branch (if not Travis) and master dependencies.

--- install.sh
+++ install.sh
@@ -509,9 +509,9 @@ build_all()
     build_from_github zeromq libzmq master $PARALLEL "$@" $ZMQ_OPTIONS
     build_from_github zeromq czmq master $PARALLEL "$@" $CZMQ_OPTIONS
     build_from_github zeromq czmqpp master $PARALLEL "$@" $CZMQPP_OPTIONS
-    build_from_github libbitcoin libbitcoin version2 $PARALLEL "$@" $BITCOIN_OPTIONS
-    build_from_github libbitcoin libbitcoin-protocol version2 $PARALLEL "$@" $BITCOIN_PROTOCOL_OPTIONS
-    build_from_travis libbitcoin-server libbitcoin-server version3 $PARALLEL "$@" $BITCOIN_SERVER_OPTIONS
+    build_from_github libbitcoin libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_github libbitcoin libbitcoin-protocol master $PARALLEL "$@" $BITCOIN_PROTOCOL_OPTIONS
+    build_from_travis libbitcoin-server libbitcoin-server protocol $PARALLEL "$@" $BITCOIN_SERVER_OPTIONS
 }
 
 
