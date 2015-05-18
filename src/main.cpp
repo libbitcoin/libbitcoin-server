1208b6b06b3cfc968d5b1ce92278924d46797d1f
evoskuil <eric@voskuil.org>
2015-05-18 14:02:54 -0700

    Use new libbitcoin helper for initializing UTF8 STDIO.

--- src/main.cpp
+++ src/main.cpp
@@ -31,15 +31,7 @@ BC_USE_LIBBITCOIN_MAIN
  */
 int bc::main(int argc, char* argv[])
 {
-#ifdef _MSC_VER
-    if (_setmode(_fileno(stdin), _O_U8TEXT) == -1)
-        throw std::exception("Could not set STDIN to utf8 mode.");
-    if (_setmode(_fileno(stdout), _O_U8TEXT) == -1)
-        throw std::exception("Could not set STDOUT to utf8 mode.");
-    if (_setmode(_fileno(stderr), _O_U8TEXT) == -1)
-        throw std::exception("Could not set STDERR to utf8 mode.");
-#endif
-
+    bc::set_utf8_stdio();
     bc::set_thread_priority(bc::thread_priority::high);
     return bc::server::dispatch(argc, const_cast<const char**>(argv),
         bc::cin, bc::cout, bc::cerr);
