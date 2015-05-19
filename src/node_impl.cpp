d016456ef62c9ff845c8c02fbd4181fe35936080
evoskuil <eric@voskuil.org>
2015-05-19 01:38:50 -0700

    Change thread configuration.

--- src/node_impl.cpp
+++ src/node_impl.cpp
@@ -76,10 +76,10 @@ void log_to_both(std::ostream& device, std::ofstream& file, log_level level,
 node_impl::node_impl(settings_type& config)
   : outfile_(config.debug_file.string(), log_open_mode), 
     errfile_(config.error_file.string(), log_open_mode),
-    // Threadpools and the number of threads they spawn.
-    network_pool_(8, thread_priority::normal),
-    disk_pool_(9, thread_priority::low),
-    mem_pool_(2, thread_priority::low),
+    // Threadpools, the number of threads they spawn and priorities.
+    network_pool_(1, thread_priority::normal),
+    disk_pool_(6, thread_priority::low),
+    mem_pool_(1, thread_priority::low),
     // Networking related services.
     hosts_(network_pool_),
     handshake_(network_pool_),
