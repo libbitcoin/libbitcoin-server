ff85d22b834951bb1d754d2e2076b2e93f1025f4
evoskuil <eric@voskuil.org>
2015-05-20 03:05:53 -0700

    Add stanard defines and generated published header.

---
+++ include/bitcoin/server.hpp
@@ -0,0 +1,37 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014-2015 libbitcoin-server developers (see COPYING).
+//
+//        GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY
+//
+///////////////////////////////////////////////////////////////////////////////
+#ifndef LIBBITCOIN_SERVER_HPP
+#define LIBBITCOIN_SERVER_HPP
+
+/**
+ * API Users: Include only this header. Direct use of other headers is fragile 
+ * and unsupported as header organization is subject to change.
+ *
+ * Maintainers: Do not include this header internal to this library.
+ */
+
+#include <bitcoin/node.hpp>
+#include <bitcoin/server/config.hpp>
+#include <bitcoin/server/define.hpp>
+#include <bitcoin/server/dispatch.hpp>
+#include <bitcoin/server/endpoint.hpp>
+#include <bitcoin/server/message.hpp>
+#include <bitcoin/server/node_impl.hpp>
+#include <bitcoin/server/publisher.hpp>
+#include <bitcoin/server/settings.hpp>
+#include <bitcoin/server/subscribe_manager.hpp>
+#include <bitcoin/server/version.hpp>
+#include <bitcoin/server/worker.hpp>
+#include <bitcoin/server/service/blockchain.hpp>
+#include <bitcoin/server/service/compat.hpp>
+#include <bitcoin/server/service/fetch_x.hpp>
+#include <bitcoin/server/service/fullnode.hpp>
+#include <bitcoin/server/service/protocol.hpp>
+#include <bitcoin/server/service/transaction_pool.hpp>
+#include <bitcoin/server/service/util.hpp>
+
+#endif
