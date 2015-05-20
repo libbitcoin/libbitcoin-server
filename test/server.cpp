831c8bd66fc6bb63f9c5082b7870aef4eeef7bc9
evoskuil <eric@voskuil.org>
2015-05-20 03:06:33 -0700

    Stub in test sources.

---
+++ test/server.cpp
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2011-2014 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin-protocol.
+ *
+ * libbitcoin-protocol is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <boost/test/unit_test.hpp>
+#include <bitcoin/server.hpp>
+
+using namespace bc;
+using namespace bc::chain;
+using namespace bc::network;
+using namespace bc::node;
+using namespace bc::server;
+
+struct low_thread_priority_fixture
+{
+    low_thread_priority_fixture()
+    {
+        set_thread_priority(thread_priority::lowest);
+    }
+
+    ~low_thread_priority_fixture()
+    {
+        set_thread_priority(thread_priority::normal);
+    }
+};
+
+static void uninitchain(const char prefix[])
+{
+    boost::filesystem::remove_all(prefix);
+}
+
+static void initchain(const char prefix[])
+{
+    const size_t history_height = 0;
+    const auto genesis = genesis_block();
+
+    uninitchain(prefix);
+    boost::filesystem::create_directories(prefix);
+    initialize_blockchain(prefix);
+
+    db_paths paths(prefix);
+    db_interface interface(paths, { history_height });
+
+    interface.start();
+    interface.push(genesis);
+}
+
+BOOST_FIXTURE_TEST_SUITE(server_tests, low_thread_priority_fixture)
+
+BOOST_AUTO_TEST_CASE(server_test)
+{
+    BOOST_REQUIRE(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
