c9ae8de71e5f879782977350edec8afaacceead2
Phillip Mienk <mienkphi@gmail.com>
2015-06-07 18:00:41 -0700

    Adapt to branch introduction.

--- install.sh
+++ install.sh
@@ -577,11 +577,11 @@ build_all()
     build_from_github zeromq czmq master $PARALLEL "$@" $CZMQ_OPTIONS
     build_from_github zeromq czmqpp master $PARALLEL "$@" $CZMQPP_OPTIONS
     build_from_github libbitcoin secp256k1 version3 $PARALLEL "$@" $SECP256K1_OPTIONS
-    build_from_github pmienk libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_github pmienk libbitcoin refactor_type $PARALLEL "$@" $BITCOIN_OPTIONS
     build_from_github pmienk libbitcoin-consensus master $PARALLEL "$@" $BITCOIN_CONSENSUS_OPTIONS
-    build_from_github pmienk libbitcoin-blockchain master $PARALLEL "$@" $BITCOIN_BLOCKCHAIN_OPTIONS
-    build_from_github pmienk libbitcoin-node master $PARALLEL "$@" $BITCOIN_NODE_OPTIONS
-    build_from_travis pmienk libbitcoin-server master $PARALLEL "$@" $BITCOIN_SERVER_OPTIONS
+    build_from_github pmienk libbitcoin-blockchain refactor_type $PARALLEL "$@" $BITCOIN_BLOCKCHAIN_OPTIONS
+    build_from_github pmienk libbitcoin-node refactor_type $PARALLEL "$@" $BITCOIN_NODE_OPTIONS
+    build_from_travis pmienk libbitcoin-server refactor_type $PARALLEL "$@" $BITCOIN_SERVER_OPTIONS
 }
 
 
