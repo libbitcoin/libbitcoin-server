b0ac9f817dc01bad03a2b6a334d1a91c0266d36a
evoskuil <eric@voskuil.org>
2015-08-10 00:45:27 -0700

    Use full_node vs. node to prevent confusion with channels.

--- src/dispatch.cpp
+++ src/dispatch.cpp
@@ -253,8 +253,8 @@ static console_result run(const settings_type& config, std::ostream& output,
 
     output << BS_SERVER_STARTING << std::endl;
 
-    server_node node(config);
-    publisher publish(node);
+    server_node full_node(config);
+    publisher publish(full_node);
     if (config.server.publisher_enabled)
         if (!publish.start(config))
         {
@@ -263,7 +263,7 @@ static console_result run(const settings_type& config, std::ostream& output,
 
             // This is a bit wacky, since the node hasn't been started, but
             // there is threadpool cleanup code in here.
-            node.stop();
+            full_node.stop();
             return console_result::not_started;
         }
 
@@ -273,18 +273,18 @@ static console_result run(const settings_type& config, std::ostream& output,
         if (!worker.start(config))
             error << format(BS_WORKER_START_FAIL);
 
-        subscribe_manager subscriber(node);
-        attach_api(worker, node, subscriber);
+        subscribe_manager subscriber(full_node);
+        attach_api(worker, full_node, subscriber);
     }
 
     // Start the node last so subscriptions to new blocks don't miss anything.
-    if (!node.start(config))
+    if (!full_node.start(config))
     {
         error << BS_NODE_START_FAIL << std::endl;
 
         // This is a bit wacky, since the node hasn't been started yet, but
         // there is threadpool cleanup code in here.
-        node.stop();
+        full_node.stop();
         return console_result::not_started;
     }
 
@@ -308,7 +308,7 @@ static console_result run(const settings_type& config, std::ostream& output,
         if (!publish.stop())
             error << BS_PUBLISHER_STOP_FAIL << std::endl;
 
-    if (!node.stop())
+    if (!full_node.stop())
     {
         error << BS_NODE_STOP_FAIL << std::endl;
         return console_result::failure;
