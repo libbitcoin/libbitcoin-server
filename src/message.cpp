c7b75c3a2520648a3d4326a2e455cc4286640881
evoskuil <eric@voskuil.org>
2015-09-12 20:55:22 -0700

    Accomodate to_data_chunk change to to_chunk.

--- src/message.cpp
+++ src/message.cpp
@@ -114,7 +114,7 @@ void outgoing_message::send(czmqpp::socket& socket) const
     append_str(message, command_);
 
     // [ ID ]
-    data_chunk raw_id = to_data_chunk(to_little_endian(id_));
+    auto raw_id = to_chunk(to_little_endian(id_));
     BITCOIN_ASSERT(raw_id.size() == sizeof(id_));
     message.append(raw_id);
 
