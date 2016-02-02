f77d3f397152e532ffc9f74443ed159e953f80a2
evoskuil <eric@voskuil.org>
2016-02-02 12:28:18 -0800

    Fix copyright.

--- include/bitcoin/server/define.hpp
+++ include/bitcoin/server/define.hpp
@@ -3,7 +3,7 @@
  *
  * This file is part of libbitcoin-server.
  *
- * libbitcoin-node is free software: you can redistribute it and/or
+ * libbitcoin-server is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License with
  * additional permissions to the one published by the Free Software
  * Foundation, either version 3 of the License, or (at your option)
