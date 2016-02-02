c63e428c8e70a0d57f056986fd693d0ab7eac901
evoskuil <eric@voskuil.org>
2016-02-02 12:27:04 -0800

    Fix copyright year.

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -1,9 +1,9 @@
 /**
- * Copyright (c) 2011-2018 libbitcoin developers (see AUTHORS)
+ * Copyright (c) 2011-2016 libbitcoin developers (see AUTHORS)
  *
  * This file is part of libbitcoin.
  *
- * libbitcoin is free software: you can redistribute it and/or modify
+ * libbitcoin-server is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License with
  * additional permissions to the one published by the Free Software
  * Foundation, either version 3 of the License, or (at your option)
