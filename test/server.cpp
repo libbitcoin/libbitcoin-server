4c4cb883f23ca1612464b69a16258ee58f6238f0
evoskuil <eric@voskuil.org>
2016-02-03 04:29:30 -0800

    Remove dead test code.

--- test/server.cpp
+++ test/server.cpp
@@ -17,50 +17,17 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include <boost/filesystem.hpp>
 #include <boost/test/unit_test.hpp>
 #include <bitcoin/server.hpp>
 
 using namespace bc;
-using namespace bc::blockchain;
-using namespace bc::network;
-using namespace bc::node;
-using namespace bc::server;
-using namespace boost;
 
-struct low_thread_priority_fixture
-{
-    low_thread_priority_fixture()
-    {
-        set_thread_priority(thread_priority::lowest);
-    }
-
-    ~low_thread_priority_fixture()
-    {
-        set_thread_priority(thread_priority::normal);
-    }
-};
-
-static void uninitchain(filesystem::path& path)
-{
-    boost::filesystem::remove_all(path);
-}
-
-static void initchain(filesystem::path& path)
-{
-    uninitchain(path);
-    boost::filesystem::create_directories(path);
-    database::initialize(path, mainnet_genesis_block());
-}
-
-BOOST_FIXTURE_TEST_SUITE(server_tests, low_thread_priority_fixture)
+BOOST_AUTO_TEST_SUITE(server_tests)
 
 // Just a basic test to get some coverage output.
 BOOST_AUTO_TEST_CASE(server_test)
 {
-    threadpool threads;
-    threads.shutdown();
-    threads.join();
+    BOOST_REQUIRE(true);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
