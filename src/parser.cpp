70546be959d959dd88decc7bfac8d056b93a9b76
evoskuil <eric@voskuil.org>
2016-06-05 20:02:48 -0700

    Config file text.

--- src/parser.cpp
+++ src/parser.cpp
@@ -122,7 +122,7 @@ options_metadata parser::load_settings()
     (
         "network.threads",
         value<uint32_t>(&configured.network.threads),
-        "The number of threads in the network threadpool, defaults to 50."
+        "The number of threads in the application threadpool, defaults to 50."
     )
     (
         "network.identifier",
