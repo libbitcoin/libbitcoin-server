799c25e2fa6949a7cec37e5950b555261691ffa3
evoskuil <eric@voskuil.org>
2017-02-09 14:22:24 -0800

    Update version3 branch installer to build from version3 branches.

--- build.cmd
+++ build.cmd
@@ -20,39 +20,39 @@ IF NOT EXIST "%nuget_pkg_path%" (
   )
 )
 
-call :init libbitcoin libbitcoin master
+call :init libbitcoin libbitcoin version3
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository libbitcoin libbitcoin master failed."
+  call :failure "Initializing repository libbitcoin libbitcoin version3 failed."
   exit /b 1
 )
-call :init libbitcoin libbitcoin-consensus version2
+call :init libbitcoin libbitcoin-consensus version3
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository libbitcoin libbitcoin-consensus version2 failed."
+  call :failure "Initializing repository libbitcoin libbitcoin-consensus version3 failed."
   exit /b 1
 )
-call :init libbitcoin libbitcoin-database master
+call :init libbitcoin libbitcoin-database version3
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository libbitcoin libbitcoin-database master failed."
+  call :failure "Initializing repository libbitcoin libbitcoin-database version3 failed."
   exit /b 1
 )
-call :init libbitcoin libbitcoin-blockchain master
+call :init libbitcoin libbitcoin-blockchain version3
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository libbitcoin libbitcoin-blockchain master failed."
+  call :failure "Initializing repository libbitcoin libbitcoin-blockchain version3 failed."
   exit /b 1
 )
-call :init libbitcoin libbitcoin-network master
+call :init libbitcoin libbitcoin-network version3
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository libbitcoin libbitcoin-network master failed."
+  call :failure "Initializing repository libbitcoin libbitcoin-network version3 failed."
   exit /b 1
 )
-call :init libbitcoin libbitcoin-node master
+call :init libbitcoin libbitcoin-node version3
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository libbitcoin libbitcoin-node master failed."
+  call :failure "Initializing repository libbitcoin libbitcoin-node version3 failed."
   exit /b 1
 )
-call :init libbitcoin libbitcoin-protocol master
+call :init libbitcoin libbitcoin-protocol version3
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository libbitcoin libbitcoin-protocol master failed."
+  call :failure "Initializing repository libbitcoin libbitcoin-protocol version3 failed."
   exit /b 1
 )
 call :bld_repo libbitcoin-server
--- install.sh
+++ install.sh
@@ -717,14 +717,14 @@ build_all()
     build_from_tarball_boost $BOOST_URL $BOOST_ARCHIVE bzip2 . $PARALLEL "$BUILD_BOOST" "${BOOST_OPTIONS[@]}"
     build_from_github zeromq libzmq master $PARALLEL ${ZMQ_OPTIONS[@]} "$@"
     build_from_github libbitcoin secp256k1 version4 $PARALLEL ${SECP256K1_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin master $PARALLEL ${BITCOIN_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-consensus version2 $PARALLEL ${BITCOIN_CONSENSUS_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-database master $PARALLEL ${BITCOIN_DATABASE_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-blockchain master $PARALLEL ${BITCOIN_BLOCKCHAIN_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-network master $PARALLEL ${BITCOIN_NETWORK_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-node master $PARALLEL ${BITCOIN_NODE_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-protocol master $PARALLEL ${BITCOIN_PROTOCOL_OPTIONS[@]} "$@"
-    build_from_travis libbitcoin libbitcoin-server master $PARALLEL ${BITCOIN_SERVER_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin version3 $PARALLEL ${BITCOIN_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-consensus version3 $PARALLEL ${BITCOIN_CONSENSUS_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-database version3 $PARALLEL ${BITCOIN_DATABASE_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-blockchain version3 $PARALLEL ${BITCOIN_BLOCKCHAIN_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-network version3 $PARALLEL ${BITCOIN_NETWORK_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-node version3 $PARALLEL ${BITCOIN_NODE_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-protocol version3 $PARALLEL ${BITCOIN_PROTOCOL_OPTIONS[@]} "$@"
+    build_from_travis libbitcoin libbitcoin-server version3 $PARALLEL ${BITCOIN_SERVER_OPTIONS[@]} "$@"
 }
 
 
