73083691c8c965792793ec4a60d9232f3976b6b4
evoskuil <eric@voskuil.org>
2017-02-09 16:09:39 -0800

    Bump minor version number for minor release.

--- include/bitcoin/server/version.hpp
+++ include/bitcoin/server/version.hpp
@@ -12,9 +12,9 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define LIBBITCOIN_SERVER_VERSION "2.4.0"
+#define LIBBITCOIN_SERVER_VERSION "2.5.0"
 #define LIBBITCOIN_SERVER_MAJOR_VERSION 2
-#define LIBBITCOIN_SERVER_MINOR_VERSION 4
+#define LIBBITCOIN_SERVER_MINOR_VERSION 5
 #define LIBBITCOIN_SERVER_PATCH_VERSION 0
 
 #endif
