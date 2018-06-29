9053dd573e1673316d129861c629c9a46a60842f
Neill Miller <neillm@thecodefactory.org>
2018-06-29 12:48:36 -0400

    Update python subscription clients to python3 syntax

--- data/subscribe-block.py
+++ data/subscribe-block.py
@@ -12,7 +12,8 @@ if len(sys.argv) > 1:
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect(url)
-socket.setsockopt(zmq.SUBSCRIBE, '')
+socket.setsockopt(zmq.SUBSCRIBE, b'')
+socket.setsockopt(zmq.TCP_KEEPALIVE, 1)
     
 while True:
     # Collect the response in parts.
@@ -20,5 +21,5 @@ while True:
     height = struct.unpack('<I', socket.recv())[0]
     block_data = hexlify(socket.recv()).decode('ascii')
 
-    print '[Seq:{}] Block {} received {}'.format(
-        sequence, height, block_data)
+    print('[Seq:{}] Block {} received {}'.format(
+        sequence, height, block_data))
--- data/subscribe-tx.py
+++ data/subscribe-tx.py
@@ -12,11 +12,12 @@ if len(sys.argv) > 1:
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect(url)
-socket.setsockopt(zmq.SUBSCRIBE, '')
-    
+socket.setsockopt(zmq.SUBSCRIBE, b'')
+socket.setsockopt(zmq.TCP_KEEPALIVE, 1)
+
 while True:
     # Collect the response in parts.
     sequence = struct.unpack('<H', socket.recv())[0]
     transaction = hexlify(socket.recv()).decode('ascii')
 
-    print '[Seq:{}] Transaction received: {}'.format(sequence, transaction)
+    print('[Seq:{}] Transaction received: {}'.format(sequence, transaction))
