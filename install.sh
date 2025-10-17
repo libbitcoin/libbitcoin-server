48b4626905916bd0d28a866e469eb26fe958a35a
Phillip Mienk <mienkphi@gmail.com>
2025-10-16 21:31:17 -0700

    Regenerate artifacts.

--- install.sh
+++ install.sh
@@ -585,8 +585,6 @@ clone_from_github()
 
 create_from_github()
 {
-    push_directory "$BUILD_SRC_DIR"
-
     local ACCOUNT=$1
     local REPO=$2
     local BRANCH=$3
@@ -613,8 +611,6 @@ create_from_github()
         display_message "Cloning $FORK/$BRANCH..."
         clone_from_github "$FORK" "$BRANCH"
     fi
-
-    pop_directory
 }
 
 # Standard build from github.
