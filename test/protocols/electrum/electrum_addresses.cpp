0d3ba59f8ad77c6d19fcb7be50234960a757729b
evoskuil <eric@voskuil.org>
2026-04-20 14:13:29 -0400

    Rename source file and add stub test file.

--- test/protocols/electrum/electrum_addresses.cpp
+++ test/protocols/electrum/electrum_addresses.cpp
@@ -384,6 +384,4 @@ BOOST_AUTO_TEST_CASE(electrum__blockchain_address_list_unspent__confirmed_and_un
     BOOST_REQUIRE(point12_0 < point11_1);
 }
 
-// blockchain.address.subscribe
-
 BOOST_AUTO_TEST_SUITE_END()
