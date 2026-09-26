c75bb96545cedb2d5ce17e0ec83ef3ee2ce822a9
Eric Voskuil <eric@voskuil.org>
2026-09-26 01:18:41 -0400

    Add native configuration tests.

--- test/protocols/native/native_configuration.cpp
+++ test/protocols/native/native_configuration.cpp
@@ -19,6 +19,46 @@
 #include "../../test.hpp"
 #include "native_setup_fixture.hpp"
 
+using namespace boost::beast;
+
 BOOST_FIXTURE_TEST_SUITE(native_tests, native_ten_block_setup_fixture)
+
+// configuration
+// ----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(native__configuration__json__expected)
+{
+    const auto response = get_json("/v1/configuration?format=json");
+    BOOST_REQUIRE(response.is_object());
+
+    const auto& object = response.as_object();
+    BOOST_REQUIRE_EQUAL(object.size(), 7u);
+    BOOST_REQUIRE(object.at("address").as_bool());
+    BOOST_REQUIRE(object.at("filter").as_bool());
+    BOOST_REQUIRE(!object.at("turbo").as_bool());
+    BOOST_REQUIRE(object.at("witness").as_bool());
+    BOOST_REQUIRE(object.at("retarget").as_bool());
+    BOOST_REQUIRE(object.at("difficult").as_bool());
+    BOOST_REQUIRE_EQUAL(object.at("pruned").as_int64(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(native__configuration__text__not_acceptable)
+{
+    BOOST_REQUIRE_EQUAL(get_status("/v1/configuration?format=text"), http::status::not_acceptable);
+}
+
+BOOST_AUTO_TEST_CASE(native__configuration__data__not_acceptable)
+{
+    BOOST_REQUIRE_EQUAL(get_status("/v1/configuration?format=data"), http::status::not_acceptable);
+}
+
+BOOST_AUTO_TEST_CASE(native__ws_configuration__default__json)
+{
+    BOOST_REQUIRE(!ws_upgrade());
+
+    const auto response = ws_get_json("/v1/configuration");
+    BOOST_REQUIRE(response.is_object());
+    BOOST_REQUIRE(response.as_object().at("witness").as_bool());
+}
 
 BOOST_AUTO_TEST_SUITE_END()
